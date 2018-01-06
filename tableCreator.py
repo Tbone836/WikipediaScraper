491a22d538203a7d6e03f4f4447d33fc98238334
Thomas <tbone@thomasyim.com>
2018-01-06 10:23:05 -0800

    Final Output

--- tableCreator.py
+++ tableCreator.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 con = sqlite3.connect('webScraper.db')
 cur = con.cursor()
 sport1 = "soccer"
@@ -7,9 +8,12 @@ sport2 = "basketball"
 sport3 = "football"
 animal1 = "kangaroo"
 animal2 = "dog"
-KEYWORD_PARAMETER = "keyword"
-LINK_PARAMETER = "link"
-KEYWORD_LINKS_PARAMETER = "keyword_link"
+KEYWORD_COLUMN_PARAMETER = "keyword"
+LINK_COLUMN_PARAMETER = "link"
+KEYWORD_LINKS_PARAMETER = "keyword_linkS"
+KEYWORDS_TABLE_PARAMETER = "keywords"
+LINKS_TABLE_PARAMETER = "links"
+
 
 idvariable = 1
 #cur.execute("CREATE TABLE links(id INTEGER PRIMARY KEY, link TEXT)")
@@ -20,11 +24,18 @@ idvariable = 1
 link_id = 1
 keyword_id = 10
 
+user_input = str(raw_input("What would you like to search today? Insert here: "))
+
 def findCount(table):
-    count = cur.execute("SELECT COUNT(*) FROM ('" + table + "s')")
+    '''
+    purpose: access the number of values from a table
+    parameters: table(constant) that we want the count of.
+    return: keyword_id = this is improper naming because it is supposed to represent either keyword or link id.
+    '''
+    count = cur.execute("SELECT COUNT(*) FROM ('" + table + "')")
     rows = cur.fetchall()
     count = accessFirstValue(rows)
-    return count+1
+    return count
 
 
 def accessFirstValue(rows):
@@ -33,27 +44,22 @@ def accessFirstValue(rows):
 def accessLinks(rows):
     return rows[0]
 
-def insertFunction(table, item):
-    countkeywords = findCount(table)
-    cur.execute("INSERT INTO "+ table + "s (id," + table +") VALUES (?, ?)", (countkeywords, item.lower()))
+def insertFunction(table, column, item):
+    countkeywords = findCount(table) + 1
+    cur.execute("INSERT INTO "+ table + " (id," + column +") VALUES (?, ?)", (countkeywords, item.lower()))
     con.commit()
-insertFunction(KEYWORD_PARAMETER, sport1)
-insertFunction(KEYWORD_PARAMETER, sport2)
-insertFunction(KEYWORD_PARAMETER, sport3)
-insertFunction(KEYWORD_PARAMETER, animal1)
-insertFunction(KEYWORD_PARAMETER, animal2)
+insertFunction(KEYWORDS_TABLE_PARAMETER, KEYWORD_COLUMN_PARAMETER, sport1)
+insertFunction(KEYWORDS_TABLE_PARAMETER, KEYWORD_COLUMN_PARAMETER, sport2)
+insertFunction(KEYWORDS_TABLE_PARAMETER, KEYWORD_COLUMN_PARAMETER, sport3)
+insertFunction(KEYWORDS_TABLE_PARAMETER, KEYWORD_COLUMN_PARAMETER, animal1)
+insertFunction(KEYWORDS_TABLE_PARAMETER, KEYWORD_COLUMN_PARAMETER, animal2)
 
-insertFunction(LINK_PARAMETER, "wiki.com")
-insertFunction(LINK_PARAMETER, "espn.com")
-insertFunction(LINK_PARAMETER, "zoo.com")
-insertFunction(LINK_PARAMETER, "messi.com")
+insertFunction(LINKS_TABLE_PARAMETER, LINK_COLUMN_PARAMETER, "wiki.com")
+insertFunction(LINKS_TABLE_PARAMETER, LINK_COLUMN_PARAMETER, "espn.com")
+insertFunction(LINKS_TABLE_PARAMETER, LINK_COLUMN_PARAMETER, "zoo.com")
+insertFunction(LINKS_TABLE_PARAMETER, LINK_COLUMN_PARAMETER, "messi.com")
 
-countkeywords = findCount(KEYWORD_PARAMETER)
-countlinks = findCount(LINK_PARAMETER)
-countkeyword_links = findCount(KEYWORD_LINKS_PARAMETER)
-cur.execute("INSERT INTO keyword_links(id, keyword_id, link_id) VALUES (?,?,?)", (countkeyword_links, countkeywords, countlinks))
-con.commit()
-def select_id_From_Table(insertion, table):
+def select_id_From_Table(insertion, table, column):
     '''
     purpose: access the id from a table of a certain link or keyword
     parameters: insertion = link or keyword we are trying to find id of, table = the table we want to use (constant)
@@ -62,35 +68,82 @@ def select_id_From_Table(insertion, table):
     #This is a quick check for what our keyword or link is
     print(insertion.lower())
     #This gets the id
-    cur.execute("SELECT id FROM " + table + "s" + " WHERE "+ table + " = ('" + insertion.lower() + "')")
+    cur.execute("SELECT id FROM " + table + " WHERE "+ column + " = ('" + insertion.lower() + "')")
     #This takes all of the data it selected
     rows = cur.fetchall()
     #A check to see the format of how it is returned
+    print "rows"
     print rows
     #This finds the first value of each tuple in the returned list of tuples.
-    if len(rows) == 0:
+    if len(rows) != 0:
         keyword_id = accessFirstValue(rows)
         return keyword_id
 
 
 def buildRelationship(keyword, link):
-    countkeyword_links = findCount(KEYWORD_LINKS_PARAMETER)
-    keyword_id = select_id_From_Table(keyword, KEYWORD_PARAMETER)
-    link_id = select_id_From_Table(link, LINK_PARAMETER)
+    countkeyword_links = findCount(KEYWORD_LINKS_PARAMETER) + 1
+    keyword_id = select_id_From_Table(keyword, KEYWORDS_TABLE_PARAMETER, KEYWORD_COLUMN_PARAMETER)
+    link_id = select_id_From_Table(link, LINKS_TABLE_PARAMETER, LINK_COLUMN_PARAMETER)
     cur.execute("INSERT INTO keyword_links(id, keyword_id, link_id) VALUES (?,?,?)", (countkeyword_links, keyword_id, link_id))
-    print link_id
+    con.commit()
 
 
 buildRelationship(sport1, "wiki.com")
+buildRelationship(sport1, "messi.com")
+buildRelationship(sport1, "espn.com")
+buildRelationship(sport2, "espn.com")
+buildRelationship(sport2, "wiki.com")
+buildRelationship(sport3, "wiki.com")
+buildRelationship(sport3, "espn.com")
+buildRelationship(animal1, "wiki.com")
+buildRelationship(animal1, "zoo.com")
+buildRelationship(animal2, "wiki.com")
+buildRelationship(animal2, "zoo.com")
+
 
 
 def accessLinks(searched_keyword):
-    cur.execute("SELECT link FROM keywords INNER JOIN keyword_links ON keywords.id = keyword_links.keyword_id INNER JOIN links ON keyword_links.link_id = links.id WHERE keyword = "+searched_keyword)
+    return_links_printed = []
+    final_links_printed = []
+    #cur.execute("SELECT l.* FROM keywords AS k INNER JOIN keyword_links AS kl ON k.id = kl.keyword_id INNER JOIN links AS l ON kl.link_id = l.id WHERE k.keyword = "+searched_keyword)
+    #cur.execute("SELECT id FROM links WHERE link = 'wiki.com'")
+    cur.execute("SELECT link_id FROM keyword_links AS kl INNER JOIN keywords AS k ON kl.keyword_id = k.id WHERE k.keyword = ?", (searched_keyword,))
+    linkrows = cur.fetchall()
+    print "linkrows"
+    print linkrows
+    for item in linkrows:
+        temporary_link_id_holder = item[0]
+        cur.execute("SELECT link FROM links AS l INNER JOIN keyword_links AS kl ON l.id = kl.link_id WHERE kl.link_id = ?", (str(temporary_link_id_holder)))
+        rows = cur.fetchall()
+        for item in rows:
+            final_links_printed.append(item[0])
+        final_links_printed = set(final_links_printed)
+        final_links_printed = list(final_links_printed)
+        print("final output!!!")
+        print final_links_printed
+
+
+'''
+    cur.execute("SELECT link_id FROM keyword_links WHERE links.id = " + "'" + str(temporary_placeholder)+"'")
     linkrows = cur.fetchall()
     for item in linkrows:
-        print item[0]
+        temporary_placeholder = item[0]
+    cur.execute("SELECT keyword_id FROM keyword_links where link_id = " + "'" + str(temporary_placeholder)+"'")
+    linkrows = cur.fetchall()
+    for item in linkrows:
+        temporary_placeholder = item[0]
+    cur.execute("SELECT id FROM keywords WHERE keyword_id == " + "'" + str(temporary_placeholder)+"'")
+    linkrows = cur.fetchall()
+    for item in linkrows:
+        temporary_placeholder = item[0]
+    cur.execute("SELECT keyword FROM keywords WHERE id = " + "'" + str(temporary_placeholder)+"'")
+    linkrows = cur.fetchall()
+    for item in linkrows:
+        temporary_placeholder = item[0]
+    print temporary_placeholder
+'''
+accessLinks(user_input)
 
-accessLinks(sport1)
 
 
 
